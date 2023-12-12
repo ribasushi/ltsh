0b33d3889f4edc7243d997614a974ba9e0e49a49
Peter Rabbitson <ribasushi@protocol.ai>
2023-12-12 12:48:46 +0100

    Set a few defaults to more reasonable values

--- chain/consensus/filcns/upgrades.go
+++ chain/consensus/filcns/upgrades.go
@@ -6,7 +6,6 @@ import (
 	_ "embed"
 	"fmt"
 	"os"
-	"runtime"
 	"strconv"
 	"time"
 
@@ -70,7 +69,7 @@ var (
 
 func init() {
 	// the default calculation used for migration worker count
-	MigrationMaxWorkerCount = runtime.NumCPU()
+	MigrationMaxWorkerCount = 8
 	// check if an alternative value was request by environment
 	if mwcs := os.Getenv(EnvMigrationMaxWorkerCount); mwcs != "" {
 		mwc, err := strconv.ParseInt(mwcs, 10, 32)
